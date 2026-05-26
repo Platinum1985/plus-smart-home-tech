7ae30f33831a0d1b9ce7134f659c3e2fe1fd3328
Ishxan <ishmsu2@inbox.ru>
2026-05-26 20:58:59 +0300

    4

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/model/HubEvent.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/model/HubEvent.java
@@ -2,6 +2,8 @@ package ru.yandex.practicum.telemetry.collector.model;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
