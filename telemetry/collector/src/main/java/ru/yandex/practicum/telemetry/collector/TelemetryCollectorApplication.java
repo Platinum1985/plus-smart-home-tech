b9d7de1f6941a0db19342fc2e580dc02b6d60432
Ishxan <ishmsu2@inbox.ru>
2026-05-27 00:41:38 +0300

    вытащил main

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/TelemetryCollectorApplication.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/TelemetryCollectorApplication.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.telemetry.collector.service;
+package ru.yandex.practicum.telemetry.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
