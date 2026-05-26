b0e18b65d7c664018f83d2a6a6cae253e122554f
Ishxan <ishmsu2@inbox.ru>
2026-05-27 00:31:27 +0300

    config

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/configuration/KafkaConfig.java
@@ -0,0 +1,30 @@
+package ru.yandex.practicum.telemetry.collector.configuration;
+
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public ProducerFactory<String, GenericRecord> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, io.confluent.kafka.serializers.KafkaAvroSerializer.class);
+        configProps.put("schema.registry.url", "http://localhost:8080");
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, GenericRecord> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/service/TelemetryCollectorApplication.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/service/TelemetryCollectorApplication.java
@@ -1,42 +1,11 @@
 package ru.yandex.practicum.telemetry.collector.service;
 
-import com.fasterxml.jackson.databind.ser.std.StringSerializer;
-import org.apache.avro.generic.GenericRecord;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-import ru.yandex.practicum.telemetry.collector.model.KafkaEventProducer;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 public class TelemetryCollectorApplication {
     public static void main(String[] args) {
-        // Запускаем Spring Boot приложение
-        ConfigurableApplicationContext context = SpringApplication.run(TelemetryCollectorApplication.class, args);
-
-        // Настройка Kafka Producer после того, как Spring Boot уже запущен
-        Map<String, Object> producerConfigs = new HashMap<>();
-        producerConfigs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        producerConfigs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        producerConfigs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, io.confluent.kafka.serializers.KafkaAvroSerializer.class);
-        producerConfigs.put("schema.registry.url", "http://localhost:8080");
-
-        ProducerFactory<String, GenericRecord> producerFactory =
-                new DefaultKafkaProducerFactory<>(producerConfigs);
-
-        KafkaTemplate<String, GenericRecord> kafkaTemplate =
-                new KafkaTemplate<>(producerFactory);
-
-        // Регистрируем бин KafkaEventProducer в Spring‑контексте
-        context.getBeanFactory().registerSingleton(
-                "kafkaEventProducer",
-                new KafkaEventProducer(kafkaTemplate)
-        );
+        SpringApplication.run(TelemetryCollectorApplication.class, args);
     }
 }
