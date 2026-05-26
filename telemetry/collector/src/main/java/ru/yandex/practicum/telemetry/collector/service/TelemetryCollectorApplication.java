7820ec492c1bf9fccb6080af9079f64e31326dc2
Ishxan <ishmsu2@inbox.ru>
2026-05-26 22:00:02 +0300

    4

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/service/TelemetryCollectorApplication.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/service/TelemetryCollectorApplication.java
@@ -23,7 +23,7 @@ public class TelemetryCollectorApplication {
         Map<String, Object> producerConfigs = new HashMap<>();
         producerConfigs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         producerConfigs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        producerConfigs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        producerConfigs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaEventProducer.class);
         producerConfigs.put("schema.registry.url", "http://localhost:8080");
 
         ProducerFactory<String, GenericRecord> producerFactory =
