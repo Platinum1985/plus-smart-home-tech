5f8f056c6bb5fd4bf8395cbb04c44a671d6f71c4
Ishxan <ishmsu2@inbox.ru>
2026-05-29 22:25:13 +0300

    ser

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/configuration/KafkaConfig.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/configuration/KafkaConfig.java
@@ -17,7 +17,7 @@ public class KafkaConfig {
     public ProducerFactory<String, GenericRecord> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringSerializer);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ru.yandex.practicum.kafka.serializer.GeneralAvroSerializer);
         //configProps.put("schema.registry.url", "http://localhost:8080");
         return new DefaultKafkaProducerFactory<>(configProps);
