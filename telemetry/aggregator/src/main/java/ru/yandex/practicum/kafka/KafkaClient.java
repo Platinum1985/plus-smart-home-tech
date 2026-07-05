0e04542b923edfddf66b8226782d6d1e27202200
Ishxan <ishmsu2@inbox.ru>
2026-07-05 21:00:24 +0300

    замечания

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/kafka/KafkaClient.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/kafka/KafkaClient.java
@@ -1,12 +1,17 @@
+
 package ru.yandex.practicum.kafka;
 
 import java.util.concurrent.ExecutionException;
+
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.consumer.Consumer;
 
 public interface KafkaClient {
     KafkaProducer<String, byte[]> getProducer();
+
     Consumer<String, byte[]> getConsumer();
+
     void sendMessage(String topic, String key, byte[] value) throws InterruptedException, ExecutionException;
+
     void flush();
 }
